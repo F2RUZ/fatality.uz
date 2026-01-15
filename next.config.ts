20cf89e681f989b1057a452e2ddd843c562aaf74
F2RUZ <gaffarovferuz4@gmail.com>
2026-01-15 09:37:35 +0500

    feat: setup expert deployment for Fatality

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Static fayllar yaratish rejimini yoqadi
+  images: {
+    unoptimized: true, // Static exportda rasmlar xato bermasligi uchun
+  },
 };
 
 export default nextConfig;
